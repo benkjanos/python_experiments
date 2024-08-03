cef4b46d0a2d4cb575de8f9c4499030d7ea5e4eb
Janos Benk <benkjanos@gmail.com>
2024-08-03 23:23:30 +0200

    current status

--- threads/simple_example.py
+++ threads/simple_example.py
@@ -1,19 +1,27 @@
 import threading
 import time
 
-def thread_function(name):
-    for i in range(20):
-        print(f"Thread {name}: iteration {i}")
-        time.sleep(0)  # Yield execution to other threads
+some_int: int = 0
+
+def thread_function(name, some_int=None):
+    time.sleep(0)
+    for i in range(5):
+        some_int += 1
+        print(f"Thread {name}: iteration {i} , c {some_int}")
+        time.sleep(1e-3)  # Yield execution to other threads
     print(f"Thread {name}: finishing")
+
 
 # Create a few threads
 threads = []
-for i in range(3):
-    t = threading.Thread(target=thread_function, args=(i,))
+for i in range(8):
+    t = threading.Thread(target=thread_function, args=(i,7))
     threads.append(t)
-    t.start()
 print("main thread is doing something")
+for t in threads:
+    t.start()
+print("main thread is doing something 2")
+
 # Wait for all threads to complete
 for t in threads:
     t.join()
